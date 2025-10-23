8e9f5dc2595da06ea27f7cf0426b787e74370aab
Caio Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2025-10-23 21:29:10 +0000

    add my email send message

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -74,7 +74,7 @@ const Contact = () => {
             className="bg-gradient-to-r from-primary to-secondary hover:opacity-90 glow-purple"
             asChild
           >
-            <a href="mailto:seu@email.com">
+            <a href="mailto:caioribeirodev@gmail.com">
               <Send className="w-5 h-5 mr-2" />
               Enviar Mensagem
             </a>
